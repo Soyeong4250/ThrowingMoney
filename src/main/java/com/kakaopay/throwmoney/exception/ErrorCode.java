183da5618774b873eddb0d39380c95badeddfcef
이소영 <th42500@naver.com>
2023-04-05 17:42:25 +0900

    [Refactor] ErrorCode 추가

--- src/main/java/com/kakaopay/throwmoney/exception/ErrorCode.java
+++ src/main/java/com/kakaopay/throwmoney/exception/ErrorCode.java
@@ -10,7 +10,9 @@ public enum ErrorCode {
 
     USERNAME_NOT_FOUND(HttpStatus.NOT_FOUND, "회원을 찾을 수 없습니다."),
     CHATROOM_NOT_FOUND(HttpStatus.NOT_FOUND, "채팅방을 찾을 수 없습니다."),
-    MONEY_INFO_NOT_FOUND(HttpStatus.NOT_FOUND, "기록을 찾을 수 없습니다.");
+    MONEY_INFO_NOT_FOUND(HttpStatus.NOT_FOUND, "거래정보를 찾을 수 없습니다."),
+    FORBIDDEN_ACCESS(HttpStatus.FORBIDDEN, "접근 권한이 없습니다."),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다.");
 
     private HttpStatus status;
     private String message;
