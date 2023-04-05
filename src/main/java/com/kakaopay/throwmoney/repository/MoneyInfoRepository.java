0a15ccad90e302c8adfd768b6dd183012f490824
이소영 <th42500@naver.com>
2023-04-05 17:38:40 +0900

    [Remove] MoneyInfoRepository 제거

--- src/main/java/com/kakaopay/throwmoney/repository/MoneyInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kakaopay.throwmoney.repository;
-
-import com.kakaopay.throwmoney.domain.entity.MoneyInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MoneyInfoRepository extends JpaRepository<MoneyInfo, Long> {
-}
