0d649bd7da2b61437d85538ea11d366f34efd98c
이소영 <th42500@naver.com>
2023-04-07 15:32:34 +0900

    [Feat] BaseEntity - @MappedSuperclass
    👉 JPA  Auditing 기능을 위한 Annotation 추가

--- src/main/java/com/kakaopay/throwmoney/domain/entity/BaseEntity.java
+++ src/main/java/com/kakaopay/throwmoney/domain/entity/BaseEntity.java
@@ -2,6 +2,7 @@ package com.kakaopay.throwmoney.domain.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 
 @Getter @Setter
 @ToString
+@MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 
